14622dfa67623bf42522623a753aa5ac3aeb06e9
Adarsh <adarshadi7760@gmail.com>
2025-04-21 10:55:09 +0530

    database settings

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Add your domain name here
 ALLOWED_HOSTS = ['portfolio-5n42.onrender.com', 'localhost', '127.0.0.1', '.onrender.com']
@@ -100,18 +100,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': config('DB_NAME'),
-    #     'USER': config('DB_USER'),
-    #     'PASSWORD': config('DB_PASSWORD'),  # Replace with your PostgreSQL password
-    #     'HOST': config('DB_HOST'),
-    #     'PORT': config('DB_PORT')
-    # }
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),  # Replace with your PostgreSQL password
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT')
+    }
 }
 
 
